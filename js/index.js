facd021a458bfd0898bf151b895ca64f1233a959
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-12-02 20:29:27 -0500

    nav, button, code

--- js/index.js
+++ js/index.js
@@ -46,6 +46,27 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
+//nav
 const navItems = document.querySelectorAll("nav a");
-
 console.log(navItems);
+
+navItems[0].textContent = siteContent.nav["nav-item-1"];
+navItems[1].textContent = siteContent.nav["nav-item-2"];
+navItems[2].textContent = siteContent.nav["nav-item-3"];
+navItems[3].textContent = siteContent.nav["nav-item-4"];
+navItems[4].textContent = siteContent.nav["nav-item-5"];
+navItems[5].textContent = siteContent.nav["nav-item-6"];
+
+//header
+const header = document.querySelector("h1");
+header.textContent = siteContent.cta["h1"];
+
+//code
+const codeSnippet = document.getElementById("cta-img");
+codeSnippet.src = siteContent.cta["img-src"];
+
+//button
+const button = document.querySelector("button");
+button.textContent = siteContent.cta["button"];
+
+
